828276b7fab00b372cfd1e94d27901156d2ce251
Unknown <srkirkland@ucdavis.edu>
2019-08-12 15:32:25 -0700

    update tests

--- tests/ApiTests.cs
+++ tests/ApiTests.cs
@@ -7,17 +7,19 @@ namespace tests
 {
     public class ApiTests
     {
-        const string username = "";
+        const string username = "bfixcaes";
         const string password = "";
 
         [Fact]
         public async Task CanGetComputer()
         {
-            var client = new BigfixClient();
+            var client = new BigfixClient(username, password);
 
             var result = await client.Computers.Get("1677559868");
 
             Assert.True(true);
+
+            Assert.False(string.IsNullOrEmpty(result.OS));
         }
     }
 }
