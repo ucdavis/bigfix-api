45b1bc724cb93ba0d37de6885f4a382a1936b600
Unknown <srkirkland@ucdavis.edu>
2019-08-12 14:49:54 -0700

    ignore the bad cert

--- src/bigfix/BigfixClient.cs
+++ src/bigfix/BigfixClient.cs
@@ -3,17 +3,28 @@ using System.Net.Http;
 
 namespace Bigfix
 {
-    public class BigfixClient
+    public class BigfixClient : IDisposable
     {
         public string BaseUrl { get; private set; }
         public HttpClient HttpProvider { get; private set; }
 
-        public BigfixClient(string baseUrl = "https://bfix/api/")
+        public BigfixClient(string baseUrl = "https://bigfix-master1.ou.ad3.ucdavis.edu:52311/api/")
         {
-            HttpProvider = new HttpClient();
-            this.BaseUrl = BaseUrl;
+            // TOOD: workaround because bigfix ssl cert is invalid.  get them to fix
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+
+            HttpProvider = new HttpClient(httpClientHandler);
+            this.BaseUrl = baseUrl;
         }
 
         public ComputerRequests Computers => new ComputerRequests(this);
+
+        public void Dispose()
+        {
+            if (this.HttpProvider != null) {
+                this.HttpProvider.Dispose();
+            }
+        }
     }
 }
