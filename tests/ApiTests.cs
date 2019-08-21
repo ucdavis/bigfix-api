0707911e47b6d80fee559cf7ceb30f57a89e920c
Unknown <srkirkland@ucdavis.edu>
2019-08-21 11:25:19 -0700

    add more common queries

--- tests/ApiTests.cs
+++ tests/ApiTests.cs
@@ -55,14 +55,37 @@ namespace tests
         }
 
         [Fact]
-        public async Task CanUseCommonQuery()
+        public async Task CanGetComplexTupleQuery()
         {
             var client = new BigfixClient(username, password);
 
-            var query = client.Queries.Common.GetComputerByName("CAES-7TW1H12");
+            var result = await client.Queries.SearchWithGroupedResults("(id of it, values of results (it, bes property \"Identifying Number - Windows\"), values of results (it, bes property \"MAC Addresses - Windows\")) of bes computers whose (name of it equals \"CAES-7TW1H12\")");
+
+            Assert.NotEmpty(result.Tuples);
+
+            System.Console.WriteLine(result.Tuples[0].Answers.Length);
+        }
+
+        [Fact]
+        public async Task CanUseCommonGroupedQuery()
+        {
+            var client = new BigfixClient(username, password);
+
+            var query = client.Queries.Common.GroupedQueries.GetComputerByNameEquals("CAES-7TW1H12");
             var result = await client.Queries.SearchWithGroupedResults(query);
 
             Assert.NotEmpty(result.Tuples);
+        }
+
+        [Fact]
+        public async Task CanUseCommonSingleQuery()
+        {
+            var client = new BigfixClient(username, password);
+
+            var query = client.Queries.Common.Queries.GetWindowsIdentifyingNumberById("1677559868");
+            var result = await client.Queries.Search(query);
+
+            Assert.NotEmpty(result.AllAnswers);
         }
     }
 }
